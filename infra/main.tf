8e6657246a2279f92261ce10b671c3f080c500eb
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2024-07-06 14:58:53 -0300

    Finally it's working

--- infra/main.tf
+++ infra/main.tf
@@ -7,9 +7,23 @@ resource "aws_security_group" "pucpr_security" {
   description = "Permitir acesso http e acesso a internet"
 
   ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port = 8080
     to_port = 8080
     protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -21,11 +35,17 @@ resource "aws_security_group" "pucpr_security" {
   }
 }
 
+resource "aws_key_pair" "key_pair" {
+    key_name = "terraform_keypair"
+    public_key = file("~/.ssh/id_rsa.pub")
+}
+
 resource "aws_instance" "pucpr_server" {
   ami = "ami-06c68f701d8090592"
   instance_type = "t2.nano"
 
   user_data = file("user_data.sh")
+  key_name = aws_key_pair.key_pair.key_name
 
-  vpc_security_group_ids = [aws_security_group.pucpr_security.id]
+  vpc_security_group_ids = ["${aws_security_group.pucpr_security.id}"]
 }
--- infra/user_data.sh
+++ infra/user_data.sh
@@ -7,4 +7,5 @@ yum install -y docker
 service docker start
 usermod -a -G docker ec2-user
 
-docker run -p 8080:8080 sofistico/puc-api:latest
+docker pull sofistico/puc-api:latest
+docker run -p 80:8080 sofistico/puc-api:latest
